4f5796f0c8bd9b3cbbec4b39e79f01e27fd19508
yourshero <592131412@qq.com>
2018-11-01 17:44:21 +0800

    2018年11月1日17:43:47

---
+++ src/components/pages/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <p>HELLO WORLD</p>
+    <ul>
+    	<li v-for="(item,index) in datalist" :key='index'>
+    		<h1>{{item.personName}}:<span>{{item.viewName}}</span></h1>
+        <img v-bind:src="item.images" alt="">
+    	</li>
+    </ul>
+  </div>
+</template>
+<!--mockjs应用页面-->
+<script>
+import axios from 'axios' //局部引入
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      datalist:[]
+    }
+  },
+  mounted(){
+  	this.showdata()
+  },
+  methods:{
+  	showdata(){
+//		import axios from 'axios'
+  		axios.get('/user') //全局引入使用vue原型中的方法this.$http,已经把axios添加到原型中
+  		.then((res)=>{
+  			//console.log(res.data.data)
+        this.datalist = res.data.data
+        console.log(this.datalist);
+  		})
+  		.catch((err)=>{
+  			console.log('调用失败',err)
+  		})
+  	}
+  }
+}
+</script>
